25435718c5b6cb88a5d25fa4da8533048e023e7b
topalek <topalek@mail.ru>
2016-05-26 17:58:09 +0300

    add zyx-phpmailer + its config

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -12,6 +12,24 @@ return [
     'bootstrap' => ['log'],
     'controllerNamespace' => 'frontend\controllers',
     'components' => [
+
+        'components' => [
+            'mail' => [
+                'class'            => 'zyx\phpmailer\Mailer',
+                'viewPath'         => '@common/mail',
+                'useFileTransport' => false,
+                'config'           => [
+                    'mailer'     => 'smtp',
+                    'host'       => 'smtp.yandex.ru',
+                    'port'       => '465',
+                    'smtpsecure' => 'ssl',
+                    'smtpauth'   => true,
+                    'username'   => 'mysmtplogin@example.ru',
+                    'password'   => 'mYsmTpPassword',
+                ],
+            ],
+        ],
+        
         'user' => [
             'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
