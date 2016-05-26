8f249109895103480db11823a5cbabc05b059c6f
topalek <topalek@mail.ru>
2016-05-26 16:35:12 +0300

    initial commit

---
+++ common/config/main.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+
+        'urlManager' => [
+            'showScriptName' => false,  // Disable index.php
+            'enablePrettyUrl' => true,  // Disable r= routes
+        ],
+
+    ],
+
+];
