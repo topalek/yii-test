85a9646148891cd1eb5274f696312dff53177679
topalek <topalek@mail.ru>
2016-05-27 16:42:47 +0300

    add meta Tags

--- frontend/views/layouts/bootstrap.php
+++ frontend/views/layouts/bootstrap.php
@@ -1,38 +1,18 @@
-
+<?php
+\frontend\assets\MainAsset::register($this);
+ ?>
+<?php $this->beginPage(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Realestate Bootstrap Theme </title>
-    <meta charset="UTF-8" />
+    <title><?php Yii::$app->name ?></title>
+    <meta charset="<?php Yii::$app->charset ?>" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <?php \yii\helpers\Html::csrfMetaTags(); ?>
 
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.css" />
-    <link rel="stylesheet" href="assets/style.css"/>
-    <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-    <script src="assets/bootstrap/js/bootstrap.js"></script>
-    <script src="assets/script.js"></script>
-
-
-
-    <!-- Owl stylesheet -->
-    <link rel="stylesheet" href="assets/owl-carousel/owl.carousel.css">
-    <link rel="stylesheet" href="assets/owl-carousel/owl.theme.css">
-    <script src="assets/owl-carousel/owl.carousel.js"></script>
-    <!-- Owl stylesheet -->
-
-
-    <!-- slitslider -->
-    <link rel="stylesheet" type="text/css" href="assets/slitslider/css/style.css" />
-    <link rel="stylesheet" type="text/css" href="assets/slitslider/css/custom.css" />
-    <script type="text/javascript" src="assets/slitslider/js/modernizr.custom.79639.js"></script>
-    <script type="text/javascript" src="assets/slitslider/js/jquery.ba-cond.min.js"></script>
-    <script type="text/javascript" src="assets/slitslider/js/jquery.slitslider.js"></script>
-    <!-- slitslider -->
-
-    <script src='/google_analytics_auto.js'></script></head>
 
 <body>
-
+<?php $this->beginBody(); ?>
 
 <!-- Header Starts -->
 <div class="navbar-wrapper">
@@ -462,9 +442,7 @@
 <!-- /.modal -->
 
 
-
+<?php $this->endBody(); ?>
 </body>
 </html>
-
-
-
+<?php $this->endPage(); ?>
